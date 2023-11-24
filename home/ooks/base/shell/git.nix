ef711fd4ad96d28976a773c889bbb48d9be26a26
ooks-io <ooks@protonmail.com>
2023-11-24 20:52:48 +1300

    add: git option: 1password ssh commit signing

--- home/ooks/base/shell/git.nix
+++ home/ooks/base/shell/git.nix
@@ -5,6 +5,9 @@
     package = pkgs.gitAndTools.gitFull;
     userName = "ooks-io";
     userEmail = "ooks@protonmail.com";
+    extraConfig = {
+      gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+    };
   };
   
   home.packages = with pkgs; [
